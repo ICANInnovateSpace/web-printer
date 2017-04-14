9b4fa6d9a90aae961344d339044a5543071aa428
Mr-Joke <mrjokein2016@gmail.com>
2017-04-14 22:33:13 +0800

    finish the directory structure .

---
+++ src/main/java/com.ican.printer/servlet/GetUrlServlet.java
@@ -0,0 +1,22 @@
+package com.ican.printer.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by mrzhou on 17-4-14.
+ */
+public class GetUrlServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
